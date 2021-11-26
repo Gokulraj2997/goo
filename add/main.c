9960d1a62b0097020665fe1e18c8613f8bd50b22
Gokul Raj <gokul.raj@amtronics.in>
2021-11-26 16:04:15 +0530

    gtyu

--- add/main.c
+++ add/main.c
@@ -6,10 +6,7 @@ int main()
     printf("Hello world!\n");
     printf("handsome!\n");
     printf("Helloooooooo!\n");
-<<<<<<< HEAD
     printf("Hello world!gggggggggg\n");
-=======
     printf("Hello world!jgjfn\n");
->>>>>>> e82d4e91e94fe37bfbd748881b3b6c0c6ba33f37
     return 0;
 }
